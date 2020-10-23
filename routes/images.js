e2d74f8f586db782cb26276568fd26d2a62dbcf7
dani198912 <Feborcesim9>
2020-10-23 13:07:58 +0300

    deleted images

--- routes/images.js
+++ routes/images.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 require("dotenv").config();
 const dir = require('./utils')
+
 router.get("/", (req, res, next) => {
     console.log("Dirname", process.cwd())
     const fileName =`${process.cwd()}/images/360/CASA_1/living_01.jpg`;
